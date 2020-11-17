1219d73b2cbee53d914bbb0b4eb2826f7b62a398
NickBaynham <nickbaynham@gmail.com>
2020-11-17 10:49:44 -0500

    Deploy AWS Web Server

--- main.tf
+++ main.tf
@@ -7,7 +7,25 @@ provider "aws" {
 resource "aws_instance" server_1 {
   ami = "ami-0a91cd140a1fc148a"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+  		#!/bin/bash
+                echo "Hello from AWS" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
   tags = {
-    name = "ubuntu-1"
+    Name = "webserver1"
+  }
+}
+
+resource "aws_security_group" instance {
+  name = "webserver1-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
